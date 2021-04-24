9fcbbd792ad708a6021fccf05a73e7f0f916862f
Elvira Semenova <shalopayka@gmail.com>
2021-04-24 13:03:26 +0300

    fexed some

--- src/main/java/HW2/Animal/Animal.java
+++ src/main/java/HW2/Animal/Animal.java
@@ -6,11 +6,8 @@ public abstract class Animal{
 
     protected int satiety;
     protected final String name;
-
     protected Animal(String name) {
         this.name = name;
     }
-
-
     public abstract void eat(Food food);
 }
